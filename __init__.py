2bb64c2a45dc042f0815777daaac433885e4d082
akanksha-singh <akanksha.singh@openlabs.co.in>
2013-04-03 16:50:15 +0530

    Model and tryton views for activity stream #1016
    Adds models for
    
    * activity.stream.object
    * activity.stream
    
    Task ID: project-154/task-1016
    Review ID:9966028

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: GPLv3, see LICENSE for more details.
+"""
+import activity_stream
---
+++ tests/test_activity_stream.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+    Test activity stream
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: GPLv3, see LICENSE for more details.
+"""
+import sys
+import os
+DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
+    '..', '..', '..', '..', '..', 'trytond')))
+if os.path.isdir(DIR):
+    sys.path.insert(0, os.path.dirname(DIR))
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import POOL, CONTEXT, USER, DB_NAME
+from trytond.transaction import Transaction
+
+
+class ActivityStreamTestCase(unittest.TestCase):
+    '''
+    Test Nereid Activity Stream  module.
+    '''
+
+    def setUp(self):
+        trytond.tests.test_tryton.install_module('nereid_activity_stream')
+        self.activity_stream_obj = POOL.get('activity.stream')
+        self.party_obj = POOL.get('party.party')
+        self.company_obj = POOL.get('company.company')
+        self.nereid_user_obj = POOL.get('nereid.user')
+        self.currency_obj = POOL.get('currency.currency')
+        self.activity_stream_object_obj = POOL.get('activity.stream.object')
+        self.model_obj = POOL.get('ir.model')
+
+    def test0005views(self):
+        '''
+        Test views.
+        '''
+        test_view('nereid_activity_stream')
+
+    def test0006depends(self):
+        '''
+        Test depends.
+        '''
+        test_depends()
+
+    def create_user(self):
+        '''
+        Cretae User.
+        '''
+
+        self.user_party = self.party_obj.create({
+            'name': 'User1',
+        })
+
+        currency = self.currency_obj.create({
+            'name': 'US Dollar',
+            'code': 'USD',
+            'symbol': '$',
+        })
+
+        self.company = self.company_obj.create({
+            'name': 'openlabs',
+            'currency': currency,
+        })
+
+        actor_party = self.party_obj.create({
+            'name': 'Party1',
+        })
+
+        nereid_user_user = self.nereid_user_obj.create({
+            'party': self.user_party,
+            'company': self.company,
+        })
+
+        self.nereid_user_actor = self.nereid_user_obj.create({
+            'party': actor_party,
+            'company': self.company,
+        })
+
+        self.nereid_stream_owner = self.nereid_user_obj.create({
+            'party': nereid_user_user,
+            'company': self.company,
+        })
+
+    def test0010activity_stream(self):
+        '''
+        Creates activity stream object and uses it in activity stream.
+        '''
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            self.create_user()
+
+            party_model_id, = self.model_obj.search([
+                ('model', '=', 'party.party')
+            ])
+
+            activity_stream_object = self.activity_stream_object_obj.create({
+                'name': 'Party',
+                'model': party_model_id,
+            })
+           
+            activity_stream = self.activity_stream_obj.create({
+                'verb': 'Added a new friend',
+                'actor': self.nereid_user_actor,
+                'nereid_user': self.nereid_stream_owner,
+                'object': 'party.party,%s' % self.user_party,
+            })
+
+            user = self.nereid_user_obj.browse(self.nereid_stream_owner)
+            self.assert_(
+                self.activity_stream_obj.browse(activity_stream) in \
+                user.activity_stream
+            )
+
+    def test0020activity_stream_object(self):
+        '''
+        Creates activity stream object but uses company in activity stream.
+        '''
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            self.create_user()
+
+            self.assertRaisesRegexp(
+                Exception,
+                '.*for the field "object" is not in the selection',
+                self.activity_stream_obj.create, {
+                    'verb': 'Added a new friend',
+                    'actor': self.nereid_user_actor,
+                    'nereid_user': self.nereid_stream_owner,
+                    'object': 'company.company,%s' % self.company,
+                }
+            )
+
+
+def suite():
+    '''
+    Test Suite
+    '''
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        ActivityStreamTestCase)
+    )
+    return suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
